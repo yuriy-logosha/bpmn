93dd72b942567afe244841ad9544921e9fa56c01
yura <yuriy.logosha@gmail.com>
2019-10-30 11:31:08 +0200

    Merged js files.

--- public/javascripts/app-viewer.js
+++ public/javascripts/app-viewer.js
@@ -6,7 +6,24 @@ requirejs.config({
     }
 });
 
-requirejs(['jquery', 'bootstrap', 'viewer'],
-    function   ($, b, m) {
+requirejs(['jquery', 'bootstrap', 'bpmn-viewer'],
+    function   ($, bootstrap, BpmnJS) {
+        $.get("/"+diagramUrl, (bpmnXML) => {
+            bpmnModeler.importXML(bpmnXML, function (err) {
+                if (err) {
+                    return console.error('could not import BPMN 2.0 diagram', err);
+                }
+                var canvas = bpmnModeler.get('canvas');
+                var overlays = bpmnModeler.get('overlays');
+                canvas.zoom('fit-viewport');
+            });
+        }, 'text');
 
+        function buildCustomModeler(id) {
+            return new BpmnJS({
+                container: '#' + id
+            });
+        }
+
+        var bpmnModeler = buildCustomModeler('canvas');
     });
