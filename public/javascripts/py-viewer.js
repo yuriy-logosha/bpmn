7355b004a547f6a75614c02e6a4b36a7db6dcc04
yura <yuriy.logosha@gmail.com>
2020-04-18 23:08:49 +0300

    Add fullscreen preview.

--- public/javascripts/py-viewer.js
+++ public/javascripts/py-viewer.js
@@ -7,13 +7,42 @@ requirejs.config({
       }]
 });
 
-requirejs(['jquery', 'bootstrap.bundle', 'codemirror', 'cm/mode/python/python'],
+requirejs(['jquery', 'bootstrap.bundle', 'codemirror', 'cm/mode/python/python', ''],
     function   ($, bootstrap, CodeMirror) {
         $.get("/"+pyfile+".py", (content) => {
             $('#canvas').text(content);
+            CodeMirror.defineOption("fullScreen", false, function(cm, val, old) {
+                if (old == CodeMirror.Init) old = false;
+                if (!old == !val) return;
+                if (val) setFullscreen(cm);
+                else setNormal(cm);
+              });
+            
+              function setFullscreen(cm) {
+                var wrap = cm.getWrapperElement();
+                cm.state.fullScreenRestore = {scrollTop: window.pageYOffset, scrollLeft: window.pageXOffset,
+                                              width: wrap.style.width, height: wrap.style.height};
+                wrap.style.width = "";
+                wrap.style.height = "auto";
+                wrap.className += " CodeMirror-fullscreen";
+                document.documentElement.style.overflow = "hidden";
+                cm.refresh();
+              }
+            
+              function setNormal(cm) {
+                var wrap = cm.getWrapperElement();
+                wrap.className = wrap.className.replace(/\s*CodeMirror-fullscreen\b/, "");
+                document.documentElement.style.overflow = "";
+                var info = cm.state.fullScreenRestore;
+                wrap.style.width = info.width; wrap.style.height = info.height;
+                window.scrollTo(info.scrollLeft, info.scrollTop);
+                cm.refresh();
+              }
+
             CodeMirror.fromTextArea(document.getElementById("canvas"), {
                 // value: content,
                 lineNumbers: true,
+                fullScreen: true,
                 mode: "python"
               });
         }, 'text');
